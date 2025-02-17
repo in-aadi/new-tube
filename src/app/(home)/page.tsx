8f6438acd24ef7faf3b64bc0e05072704d978ba4
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-02-17 22:53:15 +0530

    TRPC setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-	return <div>Videos coming soon...</div>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { PageClient } from "./client";
+
+export default async function Home() {
+	void trpc.hello.prefetch({ text: "Aditya" });
+	return (
+		<HydrateClient>
+			<Suspense fallback={<p>Loading...</p>}>
+				<ErrorBoundary fallback={<p>Error...</p>}>
+					<PageClient />
+				</ErrorBoundary>
+			</Suspense>
+		</HydrateClient>
+	);
 }
