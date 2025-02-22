21949311af364309d45cdd747d6e1594c7a72777
Aditya Sharma <3008.aditya.sharma@gmail.com>
2025-02-22 20:50:22 +0530

    Mux Integration

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { ResponsiveDialog } from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
 import { toast } from "sonner";
+import { StudioUploader } from "./studio-uploader";
 
 export const StudioUploadModal = () => {
 	const utils = trpc.useUtils();
@@ -18,17 +20,30 @@ export const StudioUploadModal = () => {
 	});
 
 	return (
-		<Button
-			variant="secondary"
-			onClick={() => create.mutate()}
-			disabled={create.isPending}
-		>
-			{create.isPending ? (
-				<Loader2Icon className="animate-spin" />
-			) : (
-				<PlusIcon />
-			)}
-			Create
-		</Button>
+		<>
+			<ResponsiveDialog
+				title="Upload a video"
+				open={!!create.data?.url}
+				onOpenChange={(open) => create.reset()}
+			>
+				{create.data?.url ? (
+					<StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+				) : (
+					<Loader2Icon />
+				)}
+			</ResponsiveDialog>
+			<Button
+				variant="secondary"
+				onClick={() => create.mutate()}
+				disabled={create.isPending}
+			>
+				{create.isPending ? (
+					<Loader2Icon className="animate-spin" />
+				) : (
+					<PlusIcon />
+				)}
+				Create
+			</Button>
+		</>
 	);
 };
